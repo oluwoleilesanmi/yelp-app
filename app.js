e7e59b3ba541e6717707497702b394f52adf8789
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-08-01 09:51:57 +0100

    Configured routes to find views campground and landing

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express from "express";
 import "dotenv/config";
 
 let app = express();
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Landing page coming soon");
+  res.render("landing");
+});
+
+app.get("/campground", (req, res) => {
+  res.render("campground");
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
