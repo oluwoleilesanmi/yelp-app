0f06a80e8bd66c185943eec4f569eac628bf6384
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-08-06 21:52:17 +0100

    Configured authentication

--- app.js
+++ app.js
@@ -2,27 +2,42 @@ import express from "express";
 import "dotenv/config";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import passport from "passport";
+import flash from "connect-flash";
+import LocalStrategy from "passport-local";
+import User from "./models/user";
+import indexRoutes from "./routes/index";
+
 
 let app = express();
-
+mongoose.connect("mongodb://localhost/yelp_app");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(flash());
 
-app.get("/", (req, res) => {
-  res.render("landing");
+app.use(
+  require("express-session")({
+    secret: "its a secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
-app.get("/campground", (req, res) => {
-  res.render("campground");
-});
-
-app.get("/campground/new", (req, res) => {
-  res.render("campground/new");
-});
-
-app.post("/campground", (req, res) => {
-  res.send("process input from form");
-});
+app.use("/", indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, () => {
   console.log(`Server is wiredup and running on port ${process.env.PORT}`);
